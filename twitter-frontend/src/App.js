11865a8968c8c977e66b9f9076f5a83ef15fb82b
Abubakir Kais <bakir_janabi@yahoo.com>
2021-08-22 01:03:31 +0400

    adding the left side bar component and importing files to app.js

--- twitter-frontend/src/App.js
+++ twitter-frontend/src/App.js
@@ -1,6 +1,8 @@
+import React, { Component } from 'react'
+import NavBar from './NavBar'
 
 export default function App() {
   return (
-   
+    <NavBar />
   );
 }
---
+++ twitter-frontend/src/LeftSideBar.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class LeftSideBar extends Component {
+    render() {
+        return (
+            <h2>Side Bar</h2>
+        )
+    }
+}
